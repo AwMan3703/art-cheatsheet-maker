3cdae048c805988f2fdaba86463164da20b5b420
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-02-25 13:21:42 +0100

    centralized element types

---
+++ js/csItems.js
@@ -0,0 +1,19 @@
+
+// Available item types
+
+const csItemTypes= {
+    // type : "path/to/icon.png"
+    date : "csItemType:DATE",
+    location : "csItemType:LOCATION",
+    commission : "csItemType:COMMISSION",
+    history : "csItemType:HISTORY",
+    size : "csItemType:SIZE",
+    description : "csItemType:DESCRIPTION",
+    style : "csItemType:STYLE",
+    colors : "csItemType:COLORS",
+    light : "csItemType:LIGHT",
+    hypothesis : "csItemType:HYPOTHESIS",
+    documents : "csItemType:DOCUMENTS",
+    texts : "csItemType:TEXTS",
+    "" : "csItemType:EMPTY-STRING"
+}
