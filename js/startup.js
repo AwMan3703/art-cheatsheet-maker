877569c815c5da5ec20e018d0ab27eba5cd719ff
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-03-11 14:00:20 +0100

    added development mode

--- js/startup.js
+++ js/startup.js
@@ -4,12 +4,24 @@
 let CONFIG = {}
 
 function checkConfig(data) {
-    if (Object.keys(data).length===0) console.warn(`Config seems to be empty (${JSON.stringify(data)}). If this is intended, you can ignore this message.`); return false;
+    // Warn if the config is empty
+    if (Object.keys(data).length===0)
+        console.warn(`Config seems to be empty (loading: ${JSON.stringify(data)}). If this is intended, you can ignore this message.`);
+
+    // Disable development mode if not on localhost and not explicitly enabled on other domains
+    if (window.location.hostname!=="localhost" && !data.client.developmentModeOutsideLocalhost) {
+        data.client.developmentMode = false
+        console.warn("Development mode was attempted outside of localhost and was disabled automatically. " +
+            "To ignore domain and keep it enabled, set [client.developmentModeOutsideLocalhost] to true in config.json")}
+
+    return data
 }
 function startup(configData) {
-    CONFIG = configData
+    console.log("Checking config data...")
+    CONFIG = checkConfig(configData)
     console.log("Config data loaded")
-    checkConfig(CONFIG)
+
+    if (CONFIG.client.developmentMode) console.info("Debug mode is enabled")
 
     const itemGenerationPanel = document.getElementById("add-element-panel")
     addItemGenerationButtons(itemGenerationPanel)
@@ -17,6 +29,8 @@ function startup(configData) {
     addItemSections(contentBody)
     const imageCarousels = document.getElementsByClassName("imageCarousel-wrapper")
     forAllElements(imageCarousels, addImageCarouselOptions)
+
+    window.onbeforeunload = (ev) => { return true }
 }
 
 console.log("Fetching config data...")
