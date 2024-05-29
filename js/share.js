40e4089bb508c825ede42fa0637d23ebd6490574
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-05-29 11:53:09 +0200

    added better mobile sharing

---
+++ js/share.js
@@ -0,0 +1,14 @@
+
+function sharePage(title, description, url) {
+    if (typeof navigator.share == "function") {
+        const shareData = {
+            title: title,
+            text: description,
+            url: url
+        }
+        navigator.share(shareData)
+    } else {
+        navigator.clipboard.writeText(url)
+        alert("Url copied to clipboard!")
+    }
+}
