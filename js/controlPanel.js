19899504e5805477f804f52b96cc394e4a9b6d82
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-09-24 18:29:55 +0200

    fixed missed translation

--- js/controlPanel.js
+++ js/controlPanel.js
@@ -3,7 +3,7 @@ function addItem(type) {
     const itemType = CONFIG.sheet.items.types[type]
     textInputDialog((text) => {
         if (!isEmptyString(text)) appendItem(type, text)
-    }, null, `${itemType.emoji} Agguingi ${itemType.names.singular}`, "content:", "Add")
+    }, null, `${itemType.emoji} Agguingi ${itemType.names.singular}`, "contenuto:", "Add")
 }
 
 function addItemGenerationButtons(parent) {
