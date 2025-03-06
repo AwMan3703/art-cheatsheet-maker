0c342b65985eac53aaf4d2e66e808744afba1204
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2025-03-06 18:36:24 +0100

    Added item generation buttons keybinds

--- js/controlPanel.js
+++ js/controlPanel.js
@@ -6,17 +6,30 @@ function addItem(type) {
     }, null, `${itemType.emoji} Aggiungi ${itemType.names.singular}`, "contenuto:", "Aggiungi")
 }
 
+const keyBindKeys = ['1','2','3','4','5','6','7','8','9','0',
+    'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+const keyBindListeners = []
 function addItemGenerationButtons(types) {
     const parent = document.getElementById("add-element-panel")
     parent.querySelectorAll('*').forEach(cn => cn.remove())
+    keyBindListeners.forEach(kbl => document.body.removeEventListener("keyup", kbl))
 
+    let counter =  0
     types.forEach(type => {
         const itemType = CONFIG.sheet.items.types[type]
         const e = document.createElement("button")
         e.ontouchend = e.onclick = _ => { addItem(type) }
+
+        e.dataset.keybind = keyBindKeys[counter]
+        const keyBindCallback = ev => { if (ev.key === e.dataset.keybind) addItem(type) }
+        document.body.addEventListener("keyup", keyBindCallback)
+        keyBindListeners.push(keyBindCallback)
+
         e.className = "item-gen-button"
         e.innerText = `${itemType.emoji} ${capitalize(itemType.names.singular)}`
         parent.appendChild(e)
+
+        counter += 1
     })
 }
 
